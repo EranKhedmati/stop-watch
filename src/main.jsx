19bc412607f2d16a2fe0c70b4f65a960a00adb92
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2024-08-03 16:13:54 +0330

    configiure store

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 // style file for tailwind css
-import "./index.css"
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Redux Provider
+import { Provider } from "react-redux";
+import { store } from "./state/store.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
